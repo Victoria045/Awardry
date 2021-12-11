07f7a28dc89b583ed799b0fa34be8aa753850dc5
Victoria045 <victoriaberyl45@gmail.com>
2021-12-11 11:37:35 +0300

    create register/login route

--- award/urls.py
+++ award/urls.py
@@ -1,5 +1,15 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
+from django.contrib.auth import views as auth_views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('profile', views.ProfileViewSet)
+
 urlpatterns = [
     path('',views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('login',auth_views.LoginView.as_view(), name='login'),
+    path('account/', include('django.contrib.auth.urls')),
+    path('profile/<username>/', views.profile, name='profile'),
 ]
