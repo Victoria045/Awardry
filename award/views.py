7a4588bf7b3d18b831a314451a46e8dc2df6ce5d
Victoria045 <victoriaberyl45@gmail.com>
2021-12-11 11:37:04 +0300

    create register view

--- award/views.py
+++ award/views.py
@@ -6,6 +6,8 @@ from .serializers import ProfileSerializer
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
+from .forms import UserRegisterForm
+from django.contrib import messages
 import random
 
 def index(request):
@@ -15,3 +17,21 @@ def index(request):
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            messages.success(request, f'Your account has been created! You are now able to log in {username}.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'registration/register.html', {'form': form})  
+
+@login_required(login_url='login')
+def profile(request, username):
+    return render(request, 'award/profile.html')
