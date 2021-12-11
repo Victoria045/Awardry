ba0a082b97ab055a8d1fd9dd3e95d0d4e57bbede
Victoria045 <victoriaberyl45@gmail.com>
2021-12-11 10:27:13 +0300

    create profileviewset

--- award/views.py
+++ award/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from rest_framework import viewsets
+from .models import Profile
+from .serializers import ProfileSerializer
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+import random
 
 def index(request):
 
     return render(request, 'award/index.html')
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
