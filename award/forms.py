e292531d96b6cec766ba3689642d0290368739ed
Victoria045 <victoriaberyl45@gmail.com>
2021-12-11 12:18:23 +0300

    create updat user form and update user profile form

--- award/forms.py
+++ award/forms.py
@@ -15,3 +15,16 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'url', 'description', 'photo', 'technologies',)
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'location', 'profile_picture', 'bio', 'contact']
