28e8b816813ff3a269dfa304549e4cb99eeec652
Victoria045 <victoriaberyl45@gmail.com>
2021-12-11 12:08:46 +0300

    create post form

--- award/forms.py
+++ award/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile
+from .models import Profile, Post
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
@@ -9,3 +9,9 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'url', 'description', 'photo', 'technologies',)
