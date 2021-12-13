423e05e7eae99ab54164d18cdb0863837641df5f
Victoria045 <victoriaberyl45@gmail.com>
2021-12-13 07:15:21 +0300

    create project and search_project views

--- award/views.py
+++ award/views.py
@@ -1,38 +1,45 @@
 from django.shortcuts import render,redirect, get_object_or_404
+from django.http import HttpResponse, Http404,HttpResponseRedirect, JsonResponse
 from django.contrib.auth.decorators import login_required
 from rest_framework import viewsets
-from .models import Profile,Post
-from .serializers import ProfileSerializer,PostSerializer
+from .models import Profile,Post, Rating
+from .serializers import ProfileSerializer,UserSerializer, PostSerializer
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
-from .forms import UserRegisterForm,PostForm
+from .forms import UserRegisterForm,PostForm,UpdateUserForm,UpdateUserProfileForm,RatingsForm
 from django.contrib import messages
 import random
 
 def index(request):
     if request.method == "POST":
-        form = PostForm(request.POST)
+        # import pdb; pdb.set_trace()
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.user = request.user
             post.save()
+            return HttpResponseRedirect(request.path_info)
     else:
         form = PostForm()
-
+    
     try:
         posts = Post.objects.all()
         posts = posts[::-1]
-        a_post = random.randint(0, len(posts)-1)
+        a_post = random.randint(0,len(posts)-1)
         random_post = posts[a_post]
         print(random_post.photo)
     except Post.DoesNotExist:
         posts = None
-    return render(request, 'award/index.html', {'posts': posts, 'form': form, 'random_post': random_post})
+    return render(request, 'award/index.html', {'posts': posts, 'form': form,'random_post': random_post})
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
@@ -63,7 +70,7 @@ def user_profile(request, username):
     params = {
         'user_prof': user_prof,
     }
-    return render(request, 'userprofile.html', params)
+    return render(request, 'award/userprofile.html', params)
 
 
 @login_required(login_url='login')
@@ -83,4 +90,65 @@ def edit_profile(request, username):
         'user_form': user_form,
         'prof_form': prof_form
     }
-    return render(request, 'edit.html', params)
+    return render(request, 'award/edit.html', params)
+
+@login_required(login_url='login')
+def project(request, post):
+    post = Post.objects.get(title=post)
+    ratings = Rating.objects.filter(user=request.user, post=post).first()
+    rating_status = None
+    if ratings is None:
+        rating_status = False
+    else:
+        rating_status = True
+    if request.method == 'POST':
+        form = RatingsForm(request.POST)
+        if form.is_valid():
+            rate = form.save(commit=False)
+            rate.user = request.user
+            rate.post = post
+            rate.save()
+            post_ratings = Rating.objects.filter(post=post)
+
+            design_ratings = [d.design for d in post_ratings]
+            design_average = sum(design_ratings) / len(design_ratings)
+
+            usability_ratings = [us.usability for us in post_ratings]
+            usability_average = sum(usability_ratings) / len(usability_ratings)
+
+            content_ratings = [content.content for content in post_ratings]
+            content_average = sum(content_ratings) / len(content_ratings)
+
+            score = (design_average + usability_average + content_average) / 3
+            print(score)
+            rate.design_average = round(design_average, 2)
+            rate.usability_average = round(usability_average, 2)
+            rate.content_average = round(content_average, 2)
+            rate.score = round(score, 2)
+            rate.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = RatingsForm()
+    params = {
+        'post': post,
+        'rating_form': form,
+        'rating_status': rating_status
+
+    }
+    return render(request, 'award/project.html', params)
+
+
+def search_project(request):
+    if request.method == 'GET':
+        title = request.GET.get("title")
+        results = Post.objects.filter(title__icontains=title).all()
+        print(results)
+        message = f'name'
+        params = {
+            'results': results,
+            'message': message
+        }
+        return render(request, 'award/results.html', params)
+    else:
+        message = "You haven't searched for any image category"
+    return render(request, 'award/results.html', {'message': message})
